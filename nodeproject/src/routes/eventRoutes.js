98112f7054cc0f2843958a654b0b1e897b6da90a
Eyad Sherif <eyadsherif97@gmail.com>
2017-07-29 14:01:56 +0000

    push before download

---
+++ nodeproject/src/routes/eventRoutes.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var eventRouter = express.Router();
+
+eventRouter.route('/')
+.get(function(req,res) {
+   res.render('events',{
+      list:['first event','2nd event','3rd event'],
+      nav: [{Link:'Services',Text:'Services'},
+            {Link:'Portfolio', Text:'Portfolio'},
+            {Link:'About',Text:'About'},
+            {Link:'Team',Text:'Portfolio'},
+            {Link:'Contact',Text:'Contact'},
+            {Link:'Events',Text:'Events'}
+      ], 
+            events : eventsData
+   });
+});
+
+
+eventRouter.route('/:id')
+.get(function(req,res) {
+   var id =   req.params.id;
+      res.render('event',{
+      list:['first event','2nd event','3rd event'],
+      nav: [{Link:'Services',Text:'Services'},
+            {Link:'Portfolio', Text:'Portfolio'},
+            {Link:'About',Text:'About'},
+            {Link:'Team',Text:'Portfolio'},
+            {Link:'Contact',Text:'Contact'},
+            {Link:'Events',Text:'Events'}
+      ],
+   events:eventsData[id]
+
+   });
+});
+
+module.exports = eventRouter;
